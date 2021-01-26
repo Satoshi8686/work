ceb24599747fc5c1934f5adb0d8ae7ac56270093
aneviola <b16b.v-tec.ek9@ezweb.ne.jp>
2021-01-26 22:38:30 +0900

    2回目です

---
+++ books/resources/views/search_2.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<h1>お気に入り投稿リスト</h1> 
+</body>
+</html>
--- books/routes/web.php
+++ books/routes/web.php
@@ -16,3 +16,52 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/fav', function () {
+    return view('favorite_post_list');
+});
+
+Route::get('/follow', function () {
+    return view('follow');
+});
+
+Route::get('/follower', function () {
+    return view('follower');
+});
+
+Route::get('/inq', function () {
+    return view('inquiry');
+});
+
+Route::get('/log', function () {
+    return view('log_in');
+});
+
+Route::get('/his', function () {
+    return view('post_his');
+});
+
+Route::get('/post', function () {
+    return view('post');
+});
+
+Route::get('/pro', function () {
+    return view('profile');
+});
+
+Route::get('/ser', function () {
+    return view('search');
+});
+
+Route::get('/ser_2', function () {
+    return view('search_2');
+});
+
+Route::get('/top', function () {
+    return view('top');
+});
+
+Route::get('/user', function () {
+    return view('user');
+});
